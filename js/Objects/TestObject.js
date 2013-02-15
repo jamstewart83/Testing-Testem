1bfe3dae3df4439877c872347cc0e7e5f6685254
Jamie Stewart <jamstewart@gmail.com>
2013-02-15 16:02:19 +0000

    Some other basic test examples

--- js/Objects/TestObject.js
+++ js/Objects/TestObject.js
@@ -2,7 +2,24 @@
  * This object exists for the purposes of a test.
  */
 function TestObject() {
+	var that = this;
+
+	this.forename = "";
+	this.surname = "";
+
+	this.name = "";
+
 	this.sayMessage = function(message){
 		return message;
+	};
+
+	this.sayName = function (){
+		return this.sayFullName();		
+	};
+
+	this.sayFullName = function(){
+		var name = that.name || that.forename + " " + that.surname ;
+
+		return that.sayMessage(name);
 	};
 }
--- js/Objects/Tests/_TestObject.js
+++ js/Objects/Tests/_TestObject.js
@@ -1,5 +1,6 @@
-describe("Testing Testem", function() {
+describe("TestObject", function() {
   var testObject = new TestObject();
+
   it("can have an instance of TestObject", function() {
     expect(testObject).not.toBe(undefined);
   });
@@ -12,5 +13,10 @@ describe("Testing Testem", function() {
   it("also has a function to say goodbye", function(){
     var message = "Goodbye";
     expect(testObject.sayMessage(message)).toBe(message);
+  });
+
+  it("also has a function to display name", function(){
+    testObject.name = "Jamie Stewart";
+    expect(testObject.sayName()).toBe("Jamie Stewart");
   });
 });
